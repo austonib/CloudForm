6ec9e372f4f14173539e60d9566b1696e7048e92
austonib <lee13011@byui.edu>
2016-06-02 15:35:41 -0600

    Added main.css and view folder with header and footer.

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Online Testing Center
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <!-- the head section -->
+    <head>
+        <title>Testing Information Sheet</title>
+        <link rel="stylesheet" type="text/css" href="/main.css">
+    </head>
+
+    <!-- the body section -->
+    <body>
+        <header><h1>Testing Information Sheet</h1></header>
